ef211b3c889e6e60b5d69fe7c25c0edd51cc040e
anchoon <92065528+anchoon@users.noreply.github.com>
2022-12-17 20:24:16 +0100

    new design

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-        function changeColor() {
+       function changeColor() {
             // Get the element with the id "text"
             var textElement = document.getElementById("text");
         
@@ -7,28 +6,4 @@
             textElement.classList.toggle("blue");
           }
         
-          setInterval(changeColor, 1000);
-        
-          var texts = [
-            "Earn",
-            "Rewards",
-            "While",
-            "Using",
-            "Our",
-            "Coin",
-          ];
-        
-          var index = 0;
-        
-          function changeText() {
-            // Get the element with the id "text"
-            var textElement = document.getElementById("text");
-        
-            // Change the text content of the element
-            textElement.textContent = texts[index];
-        
-            // Increment the index
-            index = (index + 1) % texts.length;
-          }
-        
-          setInterval(changeText, 1000);
+          setInterval(changeColor, 500);
