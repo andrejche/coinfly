2faed16540198df27890fadecae8b5cc7dbd563b
anchoon <92065528+anchoon@users.noreply.github.com>
2022-12-17 20:34:54 +0100

    test

--- script.js
+++
@@ -1,9 +0,0 @@
-       function changeColor() {
-            // Get the element with the id "text"
-            var textElement = document.getElementById("text");
-        
-            // Toggle the "blue" class on the element
-            textElement.classList.toggle("blue");
-          }
-        
-          setInterval(changeColor, 500);
