a1dd19029b5ddb1f508ae1ad45ac743c33c12cc7
andrejche <92065528+andrejche@users.noreply.github.com>
2023-01-04 21:30:48 +0100

    hjghjghjaksfjkhjsadhjkas

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('13802a2427974ba5810e464cdd18c440');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query /v2/everything
-// You must include at least one q, source, or domain
-newsapi.v2.everything({
-  q: 'bitcoin',
-  sources: 'bbc-news,the-verge',
-  domains: 'bbc.co.uk, techcrunch.com',
-  from: '2017-12-01',
-  to: '2017-12-12',
-  language: 'en',
-  sortBy: 'relevancy',
-  page: 2
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  category: 'technology',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      sources: [...]
-    }
-  */
+// A $( document ).ready() block.
+$(document).ready(function () {
+
+  var settings = {
+    "url": "https://newsapi.org/v2/everything?q=crypto&apiKey=13802a2427974ba5810e464cdd18c440",
+    "method": "GET",
+    "timeout": 0,
+  };
+
+  $.ajax(settings).done(function (response) {
+
+    var title1 = response.articles[0]['title'];
+    var image1 = response.articles[0]['urlToImage'];
+    var content1 = response.articles[0]['content'];
+    $("#image-1").attr("src", image1);
+    $('#card-title-1').append(title1);
+    $('#card-text-1').append(content1);
+
+    var title2 = response.articles[1]['title'];
+    var image2 = response.articles[1]['urlToImage'];
+    var content2 = response.articles[1]['content'];
+    $("#image-2").attr("src", image2);
+    $('#card-title-2').append(title2);
+    $('#card-text-2').append(content2);
+
+    var title3 = response.articles[2]['title'];
+    var image3 = response.articles[2]['urlToImage'];
+    var content3 = response.articles[2]['content'];
+    $("#image-3").attr("src", image3);
+    $('#card-title-3').append(title3);
+    $('#card-text-3').append(content3);
+
+
+  var obj = response.articles;
+
+ 
+
+  $.each(obj, function(key,news) {
+
+      var title = news['title'];
+      var image = news['urlToImage'];
+      var content = news['content'];
+
+     var html = `<div class="col-md-4 col-lg-4 p-3">
+                        <div class="card text-bg-dark">
+                            <img src="`+image+`">
+                            <div class="card-body">
+                                <h5 class="card-title">`+title+`</h5>
+                                <p class="card-text">`+content+` </p>
+                            </div>
+                        </div>
+                    </div>`;
+                    
+      $('#all-news').append(html);
+  });
+
+    
+  });
 });
