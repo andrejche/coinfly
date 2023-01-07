2cebf165e193272eb3958c10688ccf68face3607
anchoon <92065528+andrejche@users.noreply.github.com>
2023-01-07 19:57:50 +0100

    jhgjhg

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
 
-  var settings = {
-    "url": "https://newsapi.org/v2/everything?q=crypto&apiKey=13802a2427974ba5810e464cdd18c440",
+  var newsapi = {
+    "url": "https://min-api.cryptocompare.com/data/v2/news/?lang=EN",
     "method": "GET",
     "timeout": 0,
   };
 
-  $.ajax(settings).done(function (response) {
-
-    var title1 = response.articles[0]['title'];
-    var image1 = response.articles[0]['urlToImage'];
-    var content1 = response.articles[0]['content'];
+  $.ajax(newsapi).done(function (response) {
+    var title1 = response.Data[0]['title'];
+    var image1 = response.Data[0]['imageurl'];
+    var body1 = response.Data[0]['body'];
     $("#image-1").attr("src", image1);
     $('#card-title-1').append(title1);
-    $('#card-text-1').append(content1);
+    $('#card-text-1').append(body1);
 
-    var title2 = response.articles[1]['title'];
-    var image2 = response.articles[1]['urlToImage'];
-    var content2 = response.articles[1]['content'];
+    var title2 = response.Data[1]['title'];
+    var image2 = response.Data[1]['imageurl'];
+    var body2 = response.Data[1]['body'];
     $("#image-2").attr("src", image2);
     $('#card-title-2').append(title2);
-    $('#card-text-2').append(content2);
+    $('#card-text-2').append(body2);
 
-    var title3 = response.articles[2]['title'];
-    var image3 = response.articles[2]['urlToImage'];
-    var content3 = response.articles[2]['content'];
+    var title3 = response.Data[2]['title'];
+    var image3 = response.Data[2]['imageurl'];
+    var body3 = response.Data[2]['body'];
     $("#image-3").attr("src", image3);
     $('#card-title-3').append(title3);
-    $('#card-text-3').append(content3);
+    $('#card-text-3').append(body3);
 
 
-  var obj = response.articles;
 
- 
+    var obj = response.Data;
 
-  $.each(obj, function(key,news) {
+
+
+    $.each(obj, function (key, news) {
 
       var title = news['title'];
-      var image = news['urlToImage'];
-      var content = news['content'];
+      var image = news['imageurl'];
+      var body = news['body'];
 
-     var html = `<div class="col-md-4 col-lg-4 p-3">
+      var html = `<div class="col-md-4 col-lg-4 p-3">
                         <div class="card text-bg-dark">
-                            <img src="`+image+`">
+                            <img src="` + image + `">
                             <div class="card-body">
-                                <h5 class="card-title">`+title+`</h5>
-                                <p class="card-text">`+content+` </p>
+                                <h5 class="card-title">` + title + `</h5>
+                                <p class="card-text">` + body + ` </p>
                             </div>
                         </div>
                     </div>`;
-                    
+
       $('#all-news').append(html);
+    });
+
+
   });
 
-    
+  $.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD", function (data) {
+    $("#eth-price").html(data.USD + " USD");
+  });
+
+  $.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD", function (data) {
+    $("#btc-price").html(data.USD + " USD");
+  });
+
+  $.get("https://min-api.cryptocompare.com/data/price?fsym=SOL&tsyms=USD", function (data) {
+    $("#sol-price").html(data.USD + " USD");
   });
 });
